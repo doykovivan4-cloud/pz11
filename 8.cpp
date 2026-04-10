f1c8dbda17237d43ec9ddfaf25b1c7384403d34f
doykovivan4-cloud <doykov.ivan4@gmail.com>
2026-04-10 09:34:19 +0300

    Update 8.cpp

---
+++ 8.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+     setlocale(LC_ALL, "Russian");
+    int N;
+    cin >> N;
+
+    int i = 1;
+    while (i <= N) {
+        int j = 1;
+        while (j <= N) {
+            cout << i * j << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
