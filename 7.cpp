d21c73d0870e24d0187a388b1b6155bc1e899772
doykovivan4-cloud <doykov.ivan4@gmail.com>
2026-04-10 09:33:52 +0300

    Update 7.cpp

--- 7.cpp
+++ 7.cpp
@@ -1,15 +1,27 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int x;
-    cin >> x;
-    
-    if (x > 0 && x % 2 == 0 && x < 100) {
-        cout << "Подходит";
-    } else {
-        cout << "Не подходит";
+    setlocale(LC_ALL, "Russian");
+
+ int N;
+    cin >> N;
+
+    if (N <= 1) {
+        cout << "Не простое" << endl;
+        return 0;
     }
-    
+
+    for (int i = 2; i * i <= N; i++) {
+        if (N % i == 0) {
+            cout << "Не простое" << endl;
+            return 0;
+        }
+    }
+
+    cout << "Простое" << endl;
+
+
     return 0;
 }
