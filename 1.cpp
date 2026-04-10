56e8f2dbc7e1e200e5f0716e6322cc1a911e3226
doykovivan4-cloud <doykov.ivan4@gmail.com>
2026-04-10 09:32:31 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -3,15 +3,17 @@
 using namespace std;
 
 int main() {
-    // Настройка корректного отображения русского языка в консоли
     setlocale(LC_ALL, "Russian");
 
-    int number; // Объявляем переменную для хранения числа
-    cin >> number; // Считываем число с клавиатуры
+    int n;
+    cin >> n;
+int i = 1;
+while (i <= n) {
+    cout << i << " ";
+    i++;
+}
 
-    // Ваш код:
-if(number>0)
-cout << "Положительное";
+
 
     return 0;
 }
